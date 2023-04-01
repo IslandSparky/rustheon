b573234649978f8acde170f9df7cafad15d2ea10
IslandSparky <geiselDD@gmail.com>
2023-03-31 22:22:14 -0700

    Initial commit - skeleton only showing code flow

---
+++ src/main.rs
@@ -0,0 +1,137 @@
+/*Rustheon Raytheon 703 emulator written in Rust
+
+MIT License
+Copyright (c) 2023 Darwin Geiselbrecht
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/ustheon  - emulation of a Raytheon 703 in Rust
+
+
+const MAX_INST:i32 = 1000;         // max instructions before checking controls 
+enum Mode{
+    HALT,
+    RUN,
+    STEP
+}
+struct Memory {
+    memory:[i16;32_768]
+}
+struct Cpu {
+    mode:Mode,
+    acr:i16,
+    ixr:i16,                        // the index register and extension of the acr
+    status: u16,                    // status register
+    pcr: u16,                       // program counter
+    mbr: u16,                       // memory buffer register
+    mar: usize,                     // memory address register
+    inr: u8,                        // instruction register
+    int_req: u16,                   // interrupt request register
+    int_act: u16,                   // interrupt active register
+    int_enb: u16,                   // interrupt enabled register
+
+}
+impl Cpu{                           // create new implementation of Cpu
+    fn new() -> Self {
+        Cpu {
+            mode:Mode::HALT,
+            acr:0,
+            ixr:0,                                                
+            status:0,                      
+            pcr: 0,                        
+            mbr: 0,                       
+            mar: 0,
+            inr: 0,
+            int_req:0,                        
+            int_act: 0,                       
+            int_enb:0,   
+        }
+    }
+    // instruction execution loop, broken periodically to update console
+    fn execute(&mut self,memory:&mut Memory) {
+        let mut inst_counter = 0;               // counter for number instructions before checking console
+        'executing: loop {
+            match self.mode {
+                Mode::HALT => {
+                    break 'executing;
+                },
+                Mode::STEP => {
+                    self.decode(memory);
+                    break 'executing;
+                },
+                Mode::RUN => {
+                    self.decode(memory);
+                },
+            }
+            inst_counter += 1;
+            if inst_counter >= MAX_INST {           // time to return for console/keyboard check?
+                println! ("exiting after {} instructions pcr = {} acr = {} mem = {}"
+                            ,inst_counter,self.pcr,self.acr,memory.memory[self.pcr as usize]);
+                break 'executing;
+            }
+         }
+    }
+    // first level decoder
+    fn decode(&mut self,memory:&mut Memory) {
+        self.fetch();                        // fetch instruction into MBR and INR
+        if (self.inr & 0xf0) != 0 {
+            self.decode_mem_reference(memory);
+            return;
+        }
+        match self.inr {
+            0x00 => {self.decode_generic()},
+            0x01 => {self.decode_register()},
+            0x02 => {self.decode_din()},
+            0x03 => {self.decode_dot()},
+            0x04 => {self.decode_ixs()},
+            0x05 => {self.decode_dxs()},
+            0x06 => {self.decode_llb()},
+            0x07 => {self.decode_clb()},
+            0x08 => {self.decode_skip()},
+            0x09 => {self.decode_shift_arith()},
+            0x0A => {self.decode_shift_logical()},
+               _ => {self.illegal_instruction()}
+        }
+    }
+    fn  decode_mem_reference(&mut self,memory:&mut Memory) {
+        self.pcr += 1;
+        memory.memory[self.pcr as usize] = self.pcr as i16;   // store pcr for test only
+    }
+    fn decode_generic(&mut self){}
+    fn decode_register(&mut self){}
+    fn decode_din(&mut self){}
+    fn decode_dot(&mut self){}
+    fn decode_ixs(&mut self){}
+    fn decode_dxs(&mut self){}
+    fn decode_llb(&mut self){}
+    fn decode_clb(&mut self){}
+    fn decode_skip(&mut self){}
+    fn decode_shift_arith(&mut self){}
+    fn decode_shift_logical(&mut self){}
+    fn illegal_instruction(&mut self){}
+
+    fn fetch(&mut self){} 
+
+
+}
+
+fn main() {
+    let mut cpu = Cpu::new();
+    let mut memory:Memory= Memory{memory:[0i16;32768]};    
+    cpu.mode = Mode::RUN;
+    cpu.execute(&mut memory);    
+    println! ("PCR ={}",cpu.pcr);
+    
+}    
